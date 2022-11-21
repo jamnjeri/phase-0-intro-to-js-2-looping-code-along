f056b505af920a9f298834d964741de069c0eb06
Jamila <jamnjeri@gmail.com>
2022-11-21 14:25:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// For loop 
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+    let thanksMessage = []
+  let i=0;
+  while (i < names.length){
+      thanksMessage.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      i++;
+    }
+  return thanksMessage;
+}
+  
+//Counting Down  
+function countDown(number) {
+    while (number>=0){
+      console.log(number);
+      number--;
+    }
+
+}
